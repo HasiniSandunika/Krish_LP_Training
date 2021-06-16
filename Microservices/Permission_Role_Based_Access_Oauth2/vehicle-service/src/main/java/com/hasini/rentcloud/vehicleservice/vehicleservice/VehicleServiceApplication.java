6c1f4aaa92344dccb000995c7e591eec2f4a5299
hasinisilvacs <silvahansini@gmail.com>
2021-06-16 13:46:20 +0530

    role based access given for the vehicle-service

---
+++ Microservices/Permission_Role_Based_Access_Oauth2/vehicle-service/src/main/java/com/hasini/rentcloud/vehicleservice/vehicleservice/VehicleServiceApplication.java
@@ -0,0 +1,19 @@
+package com.hasini.rentcloud.vehicleservice.vehicleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EntityScan(basePackages = "com.hasini.rentcloud.commons.model")
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class VehicleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleServiceApplication.class, args);
+	}
+
+}
