cb566c1fce7b7d17f2a6bf74a10c611dc86a85cd
hasinisilvacs <silvahansini@gmail.com>
2021-06-17 13:30:15 +0530

    task implementation

---
+++ Microservices/Task/rent-process-task/src/main/java/com/hasini/rentcloud/rentprocesstask/service/RentProcessServiceImpl.java
@@ -0,0 +1,14 @@
+package com.hasini.rentcloud.rentprocesstask.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RentProcessServiceImpl implements RentProcessService {
+
+    @Override
+    public boolean validateDL(String dlNumber){
+        return dlNumber.length()>5;
+    }
+}
